676aad65a2db407517dc38c5dedf35de5fd8af88
Sebastian <brutzzl3r@gmx.de>
2017-06-16 15:06:31 +0200

    Created package - updated core.R

--- R/core.R
+++ R/core.R
@@ -2,7 +2,7 @@
 # READS TO TAXONOMIES AND KEGG ORTHOLOGS IE. CREATING COUNTS. CONDITIONS MAY BE PROVIDED AS
 # `1`-BASED VECTOR OF NUMBERS. THE TYPE MIGHT BE EITHER FASTA OR UPROC 
 mPathy.buildObject =
-function( files, type, mPOjFile, conditions = rep( 1, length( files ) ) )
+function( files, type, mPObjFile, conditions = rep( 1, length( files ) ) )
 {
    # TODO: @heiner - dummy stub
 }
@@ -10,7 +10,7 @@ function( files, type, mPOjFile, conditions = rep( 1, length( files ) ) )
 # WRITES KEGG-ORTHOLOG-TABLES, MAP-TABLES AND PATHWAY-MAPS BELONGING AS BROWSABLE HTML FILES TO THE
 # PROVIDED OUTPUT PATH
 mPathy.writeOutput =
-function( mPObjFile, outPath )
+function( mPObjFile, outPath, force = FALSE )
 {
    # TODO: @sebastian - dummy stub
 }
